cee834b41331eefa9aeeba79bcfc3616f9ffeb39
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-08-29 22:17:03 +0800

    Update Truckloads.cpp

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int Truckloads::numTrucks(int numCrates, int loadSize) {
-    if (numCrates <= loadSize) 
-        return 1;
+    if (numCrates <= 0 || loadSize <= 0)  return 0;
+    if (numCrates <= loadSize)  return 1;
     if (numCrates % 2 == 0) 
         return numTrucks(numCrates / 2, loadSize) + numTrucks(numCrates / 2, loadSize);
     else 
