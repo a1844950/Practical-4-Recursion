42100e9f09c7e1481acbd2bb27c63909f7e084fa
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-08-29 21:23:00 +0800

    Create Reverse.h

---
+++ Reverse.h
@@ -0,0 +1,13 @@
+#ifndef REVERSE_H
+#define REVERSE_H
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+class Reverse {
+public:
+    int reverseDigit(int value);
+    string reverseString(string letters);
+};
+#endif //REVERSE_H
