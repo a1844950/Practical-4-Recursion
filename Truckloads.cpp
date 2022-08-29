0246e65588a15bb3fe616ff21d985b29d67a983f
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-08-29 21:20:57 +0800

    Create Truckloads.cpp

---
+++ Truckloads.cpp
@@ -0,0 +1,12 @@
+#include "Truckloads.h"
+#include <iostream>
+using namespace std;
+
+int Truckloads::numTrucks(int numCrates, int loadSize) {
+    if (numCrates <= loadSize) 
+        return 1;
+    if (numCrates % 2 == 0) 
+        return numTrucks(numCrates / 2, loadSize) + numTrucks(numCrates / 2, loadSize);
+    else 
+        return numTrucks(numCrates / 2, loadSize) + numTrucks((numCrates / 2) + 1, loadSize);
+}
