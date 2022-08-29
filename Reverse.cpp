22a214eae64649e50c67ed76b1bc9e5172a52204
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-08-29 21:22:20 +0800

    Create Reverse.cpp

---
+++ Reverse.cpp
@@ -0,0 +1,24 @@
+#include "Reverse.h"
+
+
+string Reverse::reverseString(string letters){
+    size_t number_characters= letters.size();
+    if(number_characters== 1)
+        return letters;
+    else{
+        return letters[number_characters- 1]+reverseString(letters.substr(0, number_characters- 1));
+    }
+}
+
+int Reverse::reverseDigit(int value){
+
+    if (value <= 0||value < INT_MIN / 10 || value > INT_MAX / 10) {
+        return 0;
+    }
+    if(value<10) {
+        return value;
+    }
+    int len=0;
+    for (int i=value; i; i/=10) len++;
+    return value%10*(int)pow(10, len-1) + reverseDigit(value/10);
+}
