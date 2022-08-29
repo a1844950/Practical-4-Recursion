dfd59946bc759a174ac33df3914416ce73f34d32
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-08-29 21:24:06 +0800

    Create EfficientTruckloads.h

---
+++ EfficientTruckloads.h
@@ -0,0 +1,13 @@
+#ifndef EFFICIENTTRUCKLOADS_H
+#define EFFICIENTTRUCKLOADS_H
+
+#include <vector>
+class EfficientTruckloads {
+public:
+    EfficientTruckloads();
+    int numTrucks(int numCrates, int loadSize);
+    ~EfficientTruckloads();    
+};
+
+
+#endif //EFFICIENTTRUCKLOADS_H
